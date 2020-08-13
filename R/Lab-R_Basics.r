54a0d34a2a2a7a1a20bffc54ca249d3ce2e45bd7
Angel <angelr4a1@gmail.com>
2020-08-12 23:52:28 -0500

    R 101

---
+++ R/Lab-R_Basics.r
@@ -0,0 +1,38 @@
+139 + 121 
+
+260 / 60
+
+x <- 139 + 121
+
+x
+
+y <- x / 60
+y
+
+x <- x / 60
+x
+
+total <- 139 + 121
+total
+
+total_hr <- total / 60
+total_hr
+
+total_hr <- (139 + 121) / 60
+total_hr
+
+c(81, 125) / 60
+
+ratings <- c(81, 125)
+ratings / 60
+
+c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+c(1:10)
+
+c(10:1) # 10 to 1
+
+movie <- "Toy Story"
+movie
+
+genres <- c("Animation", "Comedy", "Biography", "Horror", "Romance", "Sci-fi")
+genres
